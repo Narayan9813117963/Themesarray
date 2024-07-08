5b79caf0f80ed0d5f9b7bda43db3bc87a4fb9c41
Narayan9813117963 <chaudharynarayan799@gmail.com>
2024-07-08 11:50:31 +0545

    classes added in div reduced extra css

--- inc/header.php
+++ inc/header.php
@@ -11,66 +11,66 @@
 </head>
 
 <body>
-    <div class="header position-fixed w-100">
-    <nav class="custom-container">
-        <div class="row align-items-center w-100 flex-nowrap">
-            <div class="col-6 col-lg-2">
-                <figure class="logo">
-                    <img src="../images/logo.png" alt="logo">
-                </figure>
-            </div>
-            <div class="col-lg-8 p-0 d-none d-lg-block">
-                <div class="navbar justify-content-end align-items-center">
-                    <ul class="d-flex p-0 m-0">
-                        <li>
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Demos <i class="bi bi-chevron-down"></i></a>
-                        </li>
-                        <li>
-                            <a href="#">Product pages</a>
-                        </li>
-                        <li>
-                            <a href="#">Documentation</a>
-                        </li>
-                    </ul>
+    <nav class="header position-fixed w-100">
+        <div class="custom-container">
+            <div class="row align-items-center w-100 flex-nowrap">
+                <div class="col-6 col-lg-2">
+                    <figure class="logo">
+                        <img src="../images/logo.png" alt="logo">
+                    </figure>
                 </div>
-            </div>
-            <div class="col-5 col-lg-2 p-0 d-none d-lg-flex justify-content-end">
-                <a href="#" class="d-flex primary-btn buy-btn"><i class="bi bi-bag"></i>Buy now</a>
-            </div>
-            <div class=" col-6 toggler-btn d-lg-none justify-content-end">
-                        <div class=" d-flex align-items-center justify-content-end w-100">
-                                <div class="bar-icon d-flex flex-column gap-2" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button"
-                                aria-controls="offcanvasExample">
-                                    <span class="w-100"></span>
-                                    <span class="w-100"></span>
-                                    <span class="w-100"></span>
-                                </div>
+                <div class="col-lg-8 p-0 d-none d-lg-block">
+                    <div class="navbar justify-content-end align-items-center">
+                        <ul class="d-flex p-0 m-0">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Demos <i class="bi bi-chevron-down"></i></a>
+                            </li>
+                            <li>
+                                <a href="#">Product pages</a>
+                            </li>
+                            <li>
+                                <a href="#">Documentation</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-5 col-lg-2 p-0 d-none d-lg-flex justify-content-end">
+                    <a href="#" class="d-flex primary-btn buy-btn"><i class="bi bi-bag"></i>Buy now</a>
+                </div>
+                <div class=" col-6 toggler-btn d-lg-none justify-content-end pe-0">
+                    <div class=" d-flex align-items-center justify-content-end w-100">
+                        <div class="bar-icon d-flex flex-column gap-2" data-bs-toggle="offcanvas" href="#offCanvas"
+                            role="button" aria-controls="offCanvas">
+                            <span class="w-100"></span>
+                            <span class="w-100"></span>
+                            <span class="w-100"></span>
                         </div>
+                    </div>
 
-                        <div class="offcanvas offcanvas-start hidden-nav" tabindex="-1" id="offcanvasExample"
-                            aria-labelledby="offcanvasExampleLabel">
-                            <div class="offcanvas-header">
-                                <div class="hidden-nav-logo">
-                                    <img src="../images/logo.png" alt="logo">
-                                </div>
-                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                                    aria-label="Close"></button>
+                    <div class="offcanvas offcanvas-start hidden-nav" tabindex="-1" id="offCanvas"
+                        aria-labelledby="offCanvasLabel">
+                        <div class="offcanvas-header">
+                            <div class="hidden-nav-logo">
+                                <img src="../images/logo.png" alt="logo">
                             </div>
-                            <div class="offcanvas-body">
-                                <div class="offcanvas-contents">
-                                    <ul class="d-flex flex-column p-0">
-                                        <li class="text-center"><a class="py-3 d-block  w-100" href="#">Home</a></li>
-                                        <li class="text-center"><a class="py-3 d-block  w-100" href="#">About us</a></li>
-                                        <li class="text-center"><a class="py-3 d-block  w-100" href="#">Menu</a></li>
-                                        <li class="text-center"><a class="py-3 d-block  w-100" href="#">Contact us</a></li>
-                                    </ul>
-                                </div>
+                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <div class="offcanvas-contents">
+                                <ul class="d-flex flex-column p-0">
+                                    <li class="text-center"><a class="py-3 d-block  w-100" href="#">Home</a></li>
+                                    <li class="text-center"><a class="py-3 d-block  w-100" href="#">About us</a></li>
+                                    <li class="text-center"><a class="py-3 d-block  w-100" href="#">Menu</a></li>
+                                    <li class="text-center"><a class="py-3 d-block  w-100" href="#">Contact us</a></li>
+                                </ul>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
         </div>
     </nav>
-    </div>
--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php include('inc/header.php'); ?>
 <main class="hero-section position-relative">
     <div class="custom-container">
-        <div class="hero-text position-absolute">
-            <h1>Explore No. 1<br> WordPress themes</h1>
+        <div class="hero-text position-absolute top-50">
+            <h1 class="m-0">Explore No. 1<br> WordPress themes</h1>
             <p>10+ Home Pages With Eye Catching Inner Pages. Over 80+ interface <br> elements, 10 different Live demo
                 pages.</p>
-            <a href="#" class="primary-btn d-flex align-items-center gap-2">Explore now <i class="bi bi-arrow-right"></i></a>
+            <a href="#" class="primary-btn d-flex align-items-center gap-2">Explore now <i
+                    class="bi bi-arrow-right"></i></a>
         </div>
     </div>
     <figure class="position-absolute hero-circle left-25">
@@ -20,8 +21,8 @@
 </main>
 <section class="small-feature custom-container-sm">
     <div class="feat-text text-center">
-        <h2>10+</h2>
-        <h3>Fully Customizable</h3>
+        <h2 class="m-0 fw-bold">10+</h2>
+        <h3 class="fw-bold">Fully Customizable</h3>
         <p>Beautiful, fresh templates that save you tons of time and effort to create a compelling website</p>
     </div>
     <ul class="feature-list m-0 p-0 d-flex justify-content-center flex-wrap">
@@ -55,14 +56,19 @@
     <div class="product-list">
         <div class="custom-container">
             <div class="cta filter d-flex justify-content-center align-items-center flex-wrap">
-                <a class="all active" data-filter="all" href="#" role="button"><span>All</span></a>
-                <a class="fashion" data-filter="fashion" href="#" role="button"><span>Fashion</span></a>
-                <a class="ecommerce" data-filter="ecommerce" href="#" role="button"><span>Ecommerce</span></a>
-                <a class="news" data-filter="news" href="#" role="button"><span>News</span></a>
-                <a class="sports" data-filter="sports" href="#" role="button"><span>Sports</span></a>
+                <a class="all active" data-filter="all" href="#" role="button"><span
+                        class="position-relative">All</span></a>
+                <a class="fashion" data-filter="fashion" href="#" role="button"><span
+                        class="position-relative">Fashion</span></a>
+                <a class="ecommerce" data-filter="ecommerce" href="#" role="button"><span
+                        class="position-relative">Ecommerce</span></a>
+                <a class="news" data-filter="news" href="#" role="button"><span
+                        class="position-relative">News</span></a>
+                <a class="sports" data-filter="sports" href="#" role="button"><span
+                        class="position-relative">Sports</span></a>
             </div>
             <div class="row">
-                <div class="col-12 col-md-6 col-lg-4 justify-content-center news" data-category="news">
+                <div class="col-12 col-md-6 col-lg-4 justify-content-center category-items news" data-category="news">
                     <a class="product-card d-flex flex-column justify-content-center align-items-center gap-3" href="#">
                         <figure class="product-img">
                             <img src="images/product-1.png" alt="product">
@@ -70,7 +76,8 @@
                         <p>The words</p>
                     </a>
                 </div>
-                <div class="col-12 col-md-6 col-lg-4 justify-content-center fashion" data-category="fashion">
+                <div class="col-12 col-md-6 col-lg-4 justify-content-center category-items fashion"
+                    data-category="fashion">
                     <a class="product-card d-flex flex-column justify-content-center align-items-center gap-3" href="#">
                         <figure class="product-img">
                             <img src="images/product-1.png" alt="product">
@@ -78,32 +85,35 @@
                         <p>The words</p>
                     </a>
                 </div>
-                <div class="col-12 col-md-6 col-lg-4 justify-content-center ecommerce" data-category="ecommerce">
-                    <a class="product-card d-flex flex-column justify-content-center align-items-center gap-3" href="#">
+                <div class="col-12 col-md-6 col-lg-4 justify-content-center category-items ecommerce"
+                    data-category="ecommerce">
+                    <a class="product-card flex-column justify-content-center align-items-center gap-3" href="#">
                         <figure class="product-img">
                             <img src="images/product-1.png" alt="product">
                         </figure>
                         <p>The words</p>
                     </a>
                 </div>
-                <div class="col-12 col-md-6 col-lg-4 justify-content-center fashion" data-category="fashion">
-                    <a class="product-card d-flex flex-column justify-content-center align-items-center gap-3" href="#">
+                <div class="col-12 col-md-6 col-lg-4 justify-content-center category-items fashion"
+                    data-category="fashion">
+                    <a class="product-card flex-column justify-content-center align-items-center gap-3" href="#">
                         <figure class="product-img">
                             <img src="images/product-1.png" alt="product">
                         </figure>
                         <p>The words</p>
                     </a>
                 </div>
-                <div class="col-12 col-md-6 col-lg-4 justify-content-center news" data-category="news">
-                    <a class="product-card d-flex flex-column justify-content-center align-items-center gap-3" href="#">
+                <div class="col-12 col-md-6 col-lg-4 justify-content-center category-items news" data-category="news">
+                    <a class="product-card flex-column justify-content-center align-items-center gap-3" href="#">
                         <figure class="product-img">
                             <img src="images/product-1.png" alt="product">
                         </figure>
                         <p>The words</p>
                     </a>
                 </div>
-                <div class="col-12 col-md-6 col-lg-4 justify-content-center sports" data-category="sports">
-                    <a class="product-card d-flex flex-column justify-content-center align-items-center gap-3" href="#">
+                <div class="col-12 col-md-6 col-lg-4 justify-content-center category-items sports"
+                    data-category="sports">
+                    <a class="product-card flex-column justify-content-center align-items-center gap-3" href="#">
                         <figure class="product-img">
                             <img src="images/product-1.png" alt="product">
                         </figure>
@@ -114,13 +124,14 @@
         </div>
     </div>
 </section>
-<section class="feature-section">
+<section class="feature-section section-pad">
     <div class="custom-container-sm">
-        <h2 class="text-center">Why</h2>
-        <h3 class="text-center">Choose Us for Your WordPress Themes?</h3>
+        <h2 class="text-center m-0 fw-bold">Why</h2>
+        <h3 class="text-center fw-bold m-auto">Choose Us for Your WordPress Themes?</h3>
         <div class="row">
             <div class="col-md-6">
-                <div class="feature-card mx-auto text-center d-flex flex-column align-items-center justify-content-center">
+                <div
+                    class="feature-card mx-auto text-center d-flex flex-column align-items-center justify-content-center">
                     <figure class="feature-icon">
                         <img src="images/icons/fast-load-icon.png" alt="icon">
                     </figure>
@@ -131,7 +142,8 @@
 
             </div>
             <div class="col-md-6">
-                <div class="feature-card mx-auto text-center d-flex flex-column align-items-center justify-content-center">
+                <div
+                    class="feature-card mx-auto text-center d-flex flex-column align-items-center justify-content-center">
                     <figure class="feature-icon">
                         <img src="images/icons/responsive-icon.png" alt="icon">
                     </figure>
@@ -142,7 +154,8 @@
 
             </div>
             <div class="col-md-6">
-                <div class="feature-card mx-auto text-center d-flex flex-column align-items-center justify-content-center">
+                <div
+                    class="feature-card mx-auto text-center d-flex flex-column align-items-center justify-content-center">
                     <figure class="feature-icon">
                         <img src="images/icons/import-icon.png" alt="icon">
                     </figure>
@@ -153,7 +166,8 @@
 
             </div>
             <div class="col-md-6">
-                <div class="feature-card mx-auto text-center d-flex flex-column align-items-center justify-content-center">
+                <div
+                    class="feature-card mx-auto text-center d-flex flex-column align-items-center justify-content-center">
                     <figure class="feature-icon seo-icon">
                         <img src="images/icons/seo-icon.png" alt="icon">
                     </figure>
@@ -166,14 +180,14 @@
         </div>
     </div>
 </section>
-<section class="testimonial-section position-relative">
+<section class="testimonial-section position-relative section-pad">
     <figure class="background-shape position-absolute top-0 left-0 z-n1">
         <img src="images/testimonial-shape.png" alt="shape">
     </figure>
     <div class="custom-container-md">
-        <div class="head-text text-center w-50 m-auto">
-            <h2>What</h2>
-            <h3>Client say about us</h3>
+        <div class="head-text text-center w-100 w-lg-50 m-auto">
+            <h2 class="m-0 fw-bold">What</h2>
+            <h3 class="m-0 fw-bold">Client say about us</h3>
             <p class="text-center mt-3">Get your questions answered and your issue resolved within minutes - not days!
             </p>
         </div>
@@ -249,70 +263,74 @@
 </section>
 <section class="blog-section">
     <div class="custom-container">
-        <div class="head-text text-center w-50 m-auto">
-            <h2>Read</h2>
-            <h3>latest Story</h3>
+        <div class="head-text text-center w-100 w-lg-50 m-auto">
+            <h2 class="m-0 fw-bold">Read</h2>
+            <h3 class="m-0 fw-bold">latest Story</h3>
             <p>Immerse yourself in the latest stories that will transport you to new realms, evoke emotions, and leave
                 you craving for more.</p>
         </div>
         <div class="blog-wrapper">
-        <div class="btn-for-slider d-flex justify-content-center justify-content-md-end gap-2">
+            <div class="btn-for-slider d-flex justify-content-center justify-content-md-end gap-2">
                 <div class="prev-blog-btn"><i class="bi bi-chevron-left"></i></div>
                 <div class="next-blog-btn"><i class="bi bi-chevron-right"></i></div>
             </div>
             <div class="blog-slider">
-            <div class="blog-slider-1 d-flex justify-content-center">
-                <div class="blog-card">
-                    <figure class="blog-thumb">
-                        <img src="images/blog-thumb-1.png" alt="thumb">
-                    </figure>
-                    <div class="blog-text">
-                        <span>Influencer</span>
-                        <h4>How Brands Can Break Barriers and Empower Creators</h4>
-                        <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of
-                            the… </p>
+                <div class="blog-slider-1 d-flex justify-content-center">
+                    <div class="blog-card">
+                        <figure class="blog-thumb">
+                            <img src="images/blog-thumb-1.png" alt="thumb">
+                        </figure>
+                        <div class="blog-text">
+                            <span class="fw-medium">Influencer</span>
+                            <h4>How Brands Can Break Barriers and Empower Creators</h4>
+                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end
+                                of
+                                the… </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="blog-slider-1 d-flex justify-content-center">
-                <div class="blog-card">
-                    <figure class="blog-thumb">
-                        <img src="images/blog-thumb-1.png" alt="thumb">
-                    </figure>
-                    <div class="blog-text">
-                        <span>Influencer</span>
-                        <h4>How Brands Can Break Barriers and Empower Creators</h4>
-                        <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of
-                            the… </p>
+                <div class="blog-slider-1 d-flex justify-content-center">
+                    <div class="blog-card">
+                        <figure class="blog-thumb">
+                            <img src="images/blog-thumb-1.png" alt="thumb">
+                        </figure>
+                        <div class="blog-text">
+                            <span class="fw-medium">Influencer</span>
+                            <h4>How Brands Can Break Barriers and Empower Creators</h4>
+                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end
+                                of
+                                the… </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="blog-slider-1 d-flex justify-content-center">
-                <div class="blog-card">
-                    <figure class="blog-thumb">
-                        <img src="images/blog-thumb-1.png" alt="thumb">
-                    </figure>
-                    <div class="blog-text">
-                        <span>Influencer</span>
-                        <h4>How Brands Can Break Barriers and Empower Creators</h4>
-                        <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of
-                            the… </p>
+                <div class="blog-slider-1 d-flex justify-content-center">
+                    <div class="blog-card">
+                        <figure class="blog-thumb">
+                            <img src="images/blog-thumb-1.png" alt="thumb">
+                        </figure>
+                        <div class="blog-text">
+                            <span class="fw-medium">Influencer</span>
+                            <h4>How Brands Can Break Barriers and Empower Creators</h4>
+                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end
+                                of
+                                the… </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="blog-slider-1 d-flex justify-content-center">
-                <div class="blog-card">
-                    <figure class="blog-thumb">
-                        <img src="images/blog-thumb-1.png" alt="thumb">
-                    </figure>
-                    <div class="blog-text">
-                        <span>Influencer</span>
-                        <h4>How Brands Can Break Barriers and Empower Creators</h4>
-                        <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of
-                            the… </p>
+                <div class="blog-slider-1 d-flex justify-content-center">
+                    <div class="blog-card">
+                        <figure class="blog-thumb">
+                            <img src="images/blog-thumb-1.png" alt="thumb">
+                        </figure>
+                        <div class="blog-text">
+                            <span class="fw-medium">Influencer</span>
+                            <h4>How Brands Can Break Barriers and Empower Creators</h4>
+                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end
+                                of
+                                the… </p>
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
